b96fe93cbba7aea3adfe6fce945199cc1b0d04f6
tatenodev <gd28.c.tateno@gmail.com>
2023-08-11 18:21:19 +0900

    add ImageFactory component

---
+++ src/app/components/ImageFactory.tsx
@@ -0,0 +1,9 @@
+// "use client";
+
+export const ImageFactory = () => {
+  return (
+    <div style={{ width: 400, height: 600, display: "flex" }}>
+      <div style={{ color: "black", background: "orange", width: "100%", height: "100%" }}>SVG画像</div>
+    </div>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,28 @@
 import { readFileSync } from "fs";
 import satori from "satori";
+import { ImageFactory } from "./components/ImageFactory";
 
 export default async function Home() {
   const fontFile = readFileSync("src/fonts/Noto_Sans_JP/static/NotoSansJP-Medium.ttf");
 
   const createSVG = async () => {
-    const svg = await satori(
-      <div style={{ color: "black", background: "orange", width: "100%", height: "100%" }}>SVG画像</div>,
-      {
-        width: 600,
-        height: 400,
-        fonts: [
-          {
-            name: "NotoSansJP",
-            data: await fontFile,
-          },
-        ],
-      }
-    );
+    const svg = await satori(<ImageFactory />, {
+      width: 400,
+      height: 600,
+      fonts: [
+        {
+          name: "NotoSansJP",
+          data: await fontFile,
+        },
+      ],
+    });
     return svg;
   };
 
   return (
     <main>
       <p>generate images</p>
+      <ImageFactory />
       <div dangerouslySetInnerHTML={{ __html: await createSVG() }} />
     </main>
   );
