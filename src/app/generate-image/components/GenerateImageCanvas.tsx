a3b9df8cccf2408f07a69c77e7da06c01c64d370
tatenodev <gd28.c.tateno@gmail.com>
2023-08-12 14:59:21 +0900

    add dynamic box

--- src/app/generate-image/components/GenerateImageCanvas.tsx
+++ src/app/generate-image/components/GenerateImageCanvas.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import Konva from "konva";
-import { MutableRefObject, useRef } from "react";
 import { Stage, Layer, Rect } from "react-konva";
 
 export const GenerateImageCanvas = () => {
-  const width = window.innerWidth - 100;
-  const height = window.innerHeight - 100;
-  const stageRef = useRef(null);
+  const stageWrapRef = useRef<HTMLDivElement>(null);
+  const stageRef = useRef<Konva.Stage>(null);
+  const innerWidth = window.innerWidth - 100;
+  const innerHeight = window.innerHeight - 100;
+  const { current: orangeBoxPositionX } = useRef(innerWidth);
+  const { current: orangeBoxPositionY } = useRef(innerHeight);
+  const [width, setWidth] = useState(innerWidth);
+  const [height, setHeight] = useState(innerHeight);
 
   const handleExport = (stageRef: MutableRefObject<Konva.Stage | null>) => {
     if (!stageRef.current) return;
@@ -15,17 +20,43 @@ export const GenerateImageCanvas = () => {
     console.log(uri);
   };
 
+  useEffect(() => {
+    const stageWrapElement = stageWrapRef.current;
+    if (!stageWrapElement) return;
+    const resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        setWidth(entry.contentRect.width);
+        setHeight(entry.contentRect.height);
+      }
+    });
+    resizeObserver.observe(stageWrapElement);
+
+    return () => {
+      resizeObserver.unobserve(stageWrapElement);
+    };
+  }, [stageWrapRef]);
+
   return (
     <>
       <button onClick={() => handleExport(stageRef)}>base64形式のデータをログに表示</button>
-      <Stage width={width} height={height} ref={stageRef}>
-        <Layer>
-          <Rect x={0} y={0} width={80} height={80} fill="red" />
-          <Rect x={width - 80} y={0} width={80} height={80} fill="red" />
-          <Rect x={width - 80} y={height - 80} width={80} height={80} fill="red" />
-          <Rect x={0} y={height - 80} width={80} height={80} fill="red" />
-        </Layer>
-      </Stage>
+      <div ref={stageWrapRef}>
+        <Stage width={width} height={height} ref={stageRef}>
+          <Layer>
+            <Rect x={0} y={0} width={80} height={80} fill="red" />
+            <Rect x={width - 80} y={0} width={80} height={80} fill="red" />
+            <Rect
+              draggable
+              x={orangeBoxPositionX / 2}
+              y={orangeBoxPositionY / 2}
+              width={80}
+              height={80}
+              fill="orange"
+            />
+            <Rect x={width - 80} y={height - 80} width={80} height={80} fill="red" />
+            <Rect x={0} y={height - 80} width={80} height={80} fill="red" />
+          </Layer>
+        </Stage>
+      </div>
     </>
   );
 };
