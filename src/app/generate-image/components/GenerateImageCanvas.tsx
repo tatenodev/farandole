03b4889273edb83067e5e50367a2583b8776a18d
tatenodev <gd28.c.tateno@gmail.com>
2023-08-13 15:40:40 +0900

    画像読み込み追加

--- src/app/generate-image/components/GenerateImageCanvas.tsx
+++ src/app/generate-image/components/GenerateImageCanvas.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { MutableRefObject, useEffect, useRef, useState } from "react";
+
 import Konva from "konva";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Rect, Image } from "react-konva";
+import useImage from "use-image";
+
 import { CanvasText } from "./CanvasText";
 
 export const GenerateImageCanvas = () => {
@@ -14,6 +17,9 @@ export const GenerateImageCanvas = () => {
   const { current: orangeBoxPositionY } = useRef(innerHeight);
   const [width, setWidth] = useState(innerWidth);
   const [height, setHeight] = useState(innerHeight);
+  const [imageFile, setImageFile] = useState<string | ArrayBuffer | null | undefined>(null);
+  // const [image] = useImage("https://konvajs.org/assets/lion.png");
+  const [image] = useImage(String(imageFile));
 
   const handleExport = (stageRef: MutableRefObject<Konva.Stage | null>) => {
     if (!stageRef.current) return;
@@ -40,10 +46,26 @@ export const GenerateImageCanvas = () => {
 
   return (
     <>
+      <input
+        type="file"
+        accept=".jpg,.jpeg,.png"
+        onChange={(e) => {
+          console.log(e.target.files);
+          const file = e.target.files && e.target.files[0];
+          const reader = new FileReader();
+          file && reader.readAsDataURL(file);
+          reader.onload = (e) => {
+            const base64Data = e.target?.result;
+            console.log("Base64 data:", base64Data);
+            setImageFile(base64Data);
+          };
+        }}
+      />
       <button onClick={() => handleExport(stageRef)}>base64形式のデータをログに表示</button>
       <div ref={stageWrapRef}>
         <Stage width={width} height={height} ref={stageRef}>
           <Layer>
+            <Image draggable image={image} alt="" />
             <Rect
               draggable
               x={orangeBoxPositionX / 2}
